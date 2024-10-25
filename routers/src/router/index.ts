29b14c0e2d4d425bd552d216ce46d28881a5f929
VJR <victorjacome@unicomfacauca.edu.com>
2024-10-24 21:39:35 -0500

    first commit

---
+++ routers/src/router/index.ts
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/components/Home.vue'
+import About from '@/components/About.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'about',
+   
+      //component: () => import ('@/components/About.vue')
+      component: About
+    }
+  ]
+})
+
+export default router
